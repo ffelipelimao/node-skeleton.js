ae26f1811cf9892bdc8f2da070628e0be667b6ea
limao <lflimao@hotmail.com>
2020-05-31 17:55:34 -0300

    skeleton v1.0.0

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/atendimento', (req, res) => {
+router.get('/', (req, res) => {
     res.send({
         statusCode: 200,
         message: 'Sua primeira requisição',
